0d665be53ab49d37affb917fd0f9d0f9ba78f1b4
alexjeman <alexandrujeman.ja@gmail.com>
2020-06-19 15:44:46 +0300

    Get 404 if object is not in the database

--- server/apps/learning_logs/views.py
+++ server/apps/learning_logs/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from apps.learning_logs.models import Topic, Entry
-from django.shortcuts import redirect
 from apps.learning_logs.forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
@@ -19,7 +18,7 @@ def topics(request):
 
 @login_required
 def topic(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     if topic.owner != request.user:
         raise Http404
 
